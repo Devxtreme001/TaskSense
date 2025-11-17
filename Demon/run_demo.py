2320004d34d2fa12eb4516f1f712c6dec81d7196
Devxtreme001 <dermagaming115@gmail.com>
2025-11-17 21:58:12 +0530

    Delete Demon directory

--- Demon/run_demo.py
+++
@@ -1,7 +0,0 @@
-from tasksense.agent import TaskSense
-from tasksense.config import USE_SIMULATION
-import json
-ts = TaskSense()
-task = "Update yesterday's research summary on Q3 pricing trends, add two news bullets, schedule 20-min follow-up next week."
-out = ts.run(task)
-print(json.dumps(out, indent=2))
